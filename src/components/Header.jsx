d0742cb7da87e05737ceac34da89ac22bcd3bb98
saannzhh <kalashbitch@mail.ru>
2025-04-24 22:35:16 +0500

    fix invalid href warnings for netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <div>
       <nav className="menu">
-        <a href="#">Репетиторы</a>
-        <a href="#">Как это работает</a>
-        <a href="#">Стать репетитором</a>
+        <a href="https://github.com/dtme7/tutor-online">Репетиторы</a>
+        <a href="https://github.com/dtme7/tutor-online">Как это работает</a>
+        <a href="https://github.com/dtme7/tutor-online">Стать репетитором</a>
       </nav>
     </div>
   );
